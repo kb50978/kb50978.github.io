05136bcb7f91a017b01b47b2233351d634ecfa70
kb50978 <buc16008@byui.edu>
2017-07-03 16:20:05 -0600

    week 10 try
    imcomplete search assessment

--- jquery.js
+++ jquery.js
@@ -8,7 +8,7 @@ $('#query').keyup(function() {
     $.each(data.RESULTS, function(key, val) {
       if (val.name.search(rExp) != -1) {
         output += '<li>';
-        output += '<a href="https://kb50978.github.io/Franklin/search.page.html' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
         output += '</li>';
       }
     }); // end each
@@ -16,3 +16,46 @@ $('#query').keyup(function() {
     $("#searchResults").html(output); // send results to the page
   }); // end getJSON
 }); // end onkeyup
+
+function getData(input) {
+  // Get the data from the wunderground API
+  $.ajax({
+    url: "//api.wunderground.com/api/a7b44a8b7fa643b8/geolookup/conditions/q/"
+    + input + ".json"
+    , dataType: "jsonp"
+    , success: function (data) {
+      console.log(data);
+      var location = data.location.city + ', ' + data.location.state;
+      var temp_f = data.current_observation.temp_f;
+      console.log('Location is: ' + location);
+      console.log('Temp is: ' + temp_f);
+      $("#cityDisplay").text(location);
+      $("title").html(location + " | Weather Center");
+      $("#currentTemp").html(Math.round(temp_f) + '°');
+      $("#summary").text(toTitleCase(data.current_observation.icon));
+      $("#cover").fadeOut(250);
+    }
+  });
+}
+            // Intercept the menu link clicks
+$("#page-nav").on("click", "a", function (evt) {
+  evt.preventDefault();
+  // With the text value get the needed value from the weather.json file
+  var jsonCity = $(this).text(); // Franklin, etc...
+  console.log(jsonCity);
+  $.ajax({
+    url: "/menu-activity/scripts/weather.json"
+    , dataType: "json"
+    , success: function (data) {
+      console.log(data);
+      console.log(data[jsonCity]);
+      var zip = data[jsonCity].zip;
+      console.log(zip);
+      getData(zip);
+    }
+  });
+});
+  // A function for changing a string to TitleCase
+  function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
